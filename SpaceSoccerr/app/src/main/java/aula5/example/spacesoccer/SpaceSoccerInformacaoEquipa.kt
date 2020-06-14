8f3852d98fc4737a8b1d7626b4e5018f6360370e
NunoFighT8 <a18058@alunos.ipca.pt>
2020-06-15 00:28:45 +0100

    SpaceSoccerV1

--- SpaceSoccerr/app/src/main/java/aula5/example/spacesoccer/SpaceSoccerInformacaoEquipa.kt
+++ SpaceSoccerr/app/src/main/java/aula5/example/spacesoccer/SpaceSoccerInformacaoEquipa.kt
@@ -8,5 +8,13 @@ class SpaceSoccerInformacaoEquipa : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_space_soccer_informacao_equipa)
+
+
+
+
+
+
+
+        //ola
     }
 }
