4bf2d4d209cf470380ea5cf530032413429d936c
NunoFighT8 <a18058@alunos.ipca.pt>
2020-06-15 01:30:25 +0100

    Comit By Nuno

--- SpaceSoccerr/app/src/main/java/aula5/example/spacesoccer/CriarEquipaActivity.kt
+++ SpaceSoccerr/app/src/main/java/aula5/example/spacesoccer/CriarEquipaActivity.kt
@@ -8,6 +8,5 @@ class CriarEquipaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_space_soccer_criar_equipa)
-
     }
 }
