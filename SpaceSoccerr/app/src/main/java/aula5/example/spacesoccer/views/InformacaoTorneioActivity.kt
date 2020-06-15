457ca0ae6f5faecebec7822d34aec6f2ce952487
NunoFighT8 <a18058@alunos.ipca.pt>
2020-06-15 23:15:24 +0100

    Comit by Nuno

---
+++ SpaceSoccerr/app/src/main/java/aula5/example/spacesoccer/views/InformacaoTorneioActivity.kt
@@ -0,0 +1,35 @@
+package aula5.example.spacesoccer.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import aula5.example.spacesoccer.R
+import kotlinx.android.synthetic.main.informacao_torneio.*
+
+class InformacaoTorneioActivity : AppCompatActivity() {
+
+    var idTorneio: Int? = null
+    var nomeTorneio: String? = null
+    var dtInicio: String? = null
+    var dtFim: String? = null
+    var NumEquipas: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.informacao_torneio)
+
+        val bundle = intent.extras
+        bundle?.let {
+            idTorneio = it.getInt("IdTorneio")
+            nomeTorneio = it.getString("Nome")
+            dtInicio = it.getString("dtInicio")
+            dtFim = it.getString("dtFim")
+            NumEquipas = it.getInt("NumEquipas")
+        }
+
+        txtTituloTorneio_informacaoTorneio.text = nomeTorneio
+        txtNomeTorneio2_informacaoTorneio.text = nomeTorneio
+        txtDataInicio2_informacaoTorneio.text = dtInicio
+        txtDataFim2_informacaoTorneio.text = dtFim
+        txtNumeroEquipas2_informacaoTorneio.text = NumEquipas.toString()
+    }
+}
