c396cfdaaebcc422616695f805c72519f9e31dca
NunoFighT8 <a18058@alunos.ipca.pt>
2020-06-17 15:24:31 +0100

    Comit by Nuno

--- SpaceSoccerr/app/src/main/java/aula5/example/spacesoccer/models/Jogos.kt
+++ SpaceSoccerr/app/src/main/java/aula5/example/spacesoccer/models/Jogos.kt
@@ -4,23 +4,23 @@ import org.json.JSONObject
 
 class Jogos {
 
-    var IdJogo: Int? = null
-    var EquipaCasa: String? = null
-    var EquipaConvidada: String? = null
-    var Arbitro: String? = null
-    var DataJogo: String? = null
-    var IdTorneio: Int? = null
+    var IdJogo:             Int? = null
+    var EquipaCasa:         String? = null
+    var EquipaConvidada:    String? = null
+    var Arbitro:            String? = null
+    var DataJogo:           String? = null
+    var IdTorneio:          Int? = null
 
     companion object {
         fun parseJson(jsonArticle: JSONObject): Jogos {
             val jogos = Jogos()
 
-            jogos.IdJogo = jsonArticle.getInt("IdJogo")
-            jogos.EquipaCasa = jsonArticle.getString("EquipaCasa")
-            jogos.EquipaConvidada = jsonArticle.getString("EquipaConvidada")
-            jogos.Arbitro = jsonArticle.getString("Arbitro")
-            jogos.DataJogo = jsonArticle.getString("DataJogo")
-            jogos.IdTorneio = jsonArticle.getInt("Torneio")
+            jogos.IdJogo            = jsonArticle.getInt("IdJogo")
+            jogos.EquipaCasa        = jsonArticle.getString("EquipaCasa")
+            jogos.EquipaConvidada   = jsonArticle.getString("EquipaConvidada")
+            jogos.Arbitro           = jsonArticle.getString("Arbitro")
+            jogos.DataJogo          = jsonArticle.getString("DataJogo")
+            jogos.IdTorneio         = jsonArticle.getInt("Torneio")
 
             return jogos
         }
